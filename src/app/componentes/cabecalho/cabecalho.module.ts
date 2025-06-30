a08cd4864c112e05696a4c443a373db18a098665
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-06-30 10:28:29 -0300

    importando RouterModule e exportando CabecalhoComponent

--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,11 @@ import { CabecalhoComponent } from './cabecalho.component';
     CabecalhoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    CabecalhoComponent
   ]
 })
 export class CabecalhoModule { }
